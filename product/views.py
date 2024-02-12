8c019ed3ed49a0d4566f03868b10422d40a6fd86
Aragaojorge <3glasser@gmail.com>
2024-02-12 19:45:40 -0300

    Template products details created

--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from . import models
@@ -11,8 +12,11 @@ class ProductsList(ListView):
     context_object_name = 'products'
     paginate_by = 1
 
-class ProductDetail(View):
-    pass
+class ProductDetail(DetailView):
+    model = models.Product
+    template_name = 'product/detail.html'
+    context_object_name = 'product'
+    slug_url_kwarg = 'slug'
 
 class AddToCart(View):
     pass
