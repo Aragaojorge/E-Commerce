391996502668361efa48a8a6f0d582fe2a952eb9
Aragaojorge <3glasser@gmail.com>
2024-02-13 15:01:15 -0300

    First steps to add product to the cart

--- product/views.py
+++ product/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from . import models
+from django.contrib import messages
 
 # Create your views here.
 class ProductsList(ListView):
@@ -19,7 +20,32 @@ class ProductDetail(DetailView):
     slug_url_kwarg = 'slug'
 
 class AddToCart(View):
-    pass
+    def get(self, *args, **kwargs):
+        http_referer = self.request.META.get('HTTP_REFERER', reverse('product:list'))
+        id_variation = self.request.GET.get('vid')
+        
+        if not id_variation:
+            messages.error(
+                self.request,
+                'This product does not exist!'
+            )
+            return redirect(http_referer)
+        
+        variation = get_object_or_404(models.Option, id=id_variation)
+        
+        if not self.request.session.get('cart'):
+            self.request.session['cart'] = {}
+            self.request.session.save()
+            
+        cart = self.request.session['cart']
+        
+        if id_variation in cart:
+            ...
+        else:
+            ...
+        
+        return HttpResponse('Add to cart')
+        
 
 class RemoveFromCart(View):
     pass
